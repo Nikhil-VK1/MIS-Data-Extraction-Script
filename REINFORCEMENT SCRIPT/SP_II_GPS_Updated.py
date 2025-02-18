4701866697f38ae675fcaf5df6fcbb345c405068
NIKHIL V K <nikhil_vk@goodyear.com>
2025-02-18 13:41:43 +0530

    first commit

---
+++ REINFORCEMENT SCRIPT/SP_II_GPS_Updated.py
@@ -0,0 +1,56 @@
+INFO_FILEDS_FOR_GPS =[
+    {"gui_name": "Meets Stewardship Requirements?", "ii_short_description": "ddlFullAp-GPS", "ii_order": 44000000, "measure_code": "GPS", "contains":"Dropdown"}, 
+    {"gui_name": "Comment", "ii_short_description": "mlComment", "ii_order": 2, "measure_code": "GPS", "contains":"comment"}, 
+    {"gui_name": "SDS is acceptable? ", "ii_short_description": "ddlFullAp-SDS", "ii_order": 48000000, "measure_code": "SDS", "contains":"Dropdown"}, 
+    {"gui_name": "Comment", "ii_short_description": "mlComment", "ii_order": 4, "measure_code": "SDS", "contains":"comment"},
+    {"gui_name": "Complies with substance regulations?", "ii_short_description": "ieNorthAmericaRegion", "ii_order": 21000000, "measure_code": "ieNorthAmericaRegion", "contains":"Lable"}, 
+    {"gui_name": "United States", "ii_short_description": "ddFullApp-TSCA", "ii_order": 52000000, "measure_code": "TSCA", "contains":"Dropdown"}, 
+    {"gui_name": "Comment", "ii_short_description": "mlComment", "ii_order":8, "measure_code": "TSCA", "contains":"comment"}, 
+    {"gui_name": "Canada", "ii_short_description": "ddlFullAp-CEPAD", "ii_order": 56000000, "measure_code": "CEPAD", "contains":"Dropdown"}, 
+    {"gui_name": "Comment", "ii_short_description": "mlComment", "ii_order": 10, "measure_code": "CEPAD", "contains":"comment"}, 
+    {"gui_name": "European Union", "ii_short_description": "ddlFullAp-REACHEU", "ii_order": 64000000, "measure_code": "ELINCS", "contains":"Lable"}, 
+    {"gui_name": "Comment", "ii_short_description": "mlComment", "ii_order": 13, "measure_code": "ELINCS", "contains":"comment"}, 
+    {"gui_name": "United Kingdom", "ii_short_description": "ddlFullAp-REACHUK", "ii_order": 68000000, "measure_code": "REACHUK", "contains":"Lable"}, 
+    {"gui_name": "Comment", "ii_short_description": "mlComment", "ii_order": 15, "measure_code": "REACHUK", "contains":"comment"}, 
+    {"gui_name": "Serbia", "ii_short_description": "ddlFullAp-Serbia", "ii_order": 76000000, "measure_code": "Serbia", "contains":"Lable"}, 
+    {"gui_name": "Comment", "ii_short_description": "mlComment", "ii_order": 17, "measure_code": "Serbia", "contains":"comment"}, 
+    {"gui_name": "South Africa", "ii_short_description": "ddlFullAp-SA", "ii_order": 80000000, "measure_code": "SA", "contains":"Lable"}, 
+    {"gui_name": "Comment", "ii_short_description": "mlComment", "ii_order": 19, "measure_code": "SA", "contains":"comment"}, 
+    {"gui_name": "Turkey", "ii_short_description": "ddlFullAp-KKDIK", "ii_order": 72000000, "measure_code": "KKDIK", "contains":"Lable"}, 
+    {"gui_name": "Comment", "ii_short_description": "mlComment", "ii_order": 21, "measure_code": "KKDIK", "contains":"comment"}, 
+    {"gui_name": "China", "ii_short_description": "ddlFullAp-IECSA", "ii_order": 104000000, "measure_code": "CLECS", "contains":"Lable"}, 
+    {"gui_name": "Comment", "ii_short_description": "mlComment", "ii_order": 24, "measure_code": "IECS", "contains":"comment"}, 
+    {"gui_name": "Japan", "ii_short_description": "ddlFullAp-Japan", "ii_order": 92000000, "measure_code": "ENCS", "contains":"Lable"}, 
+    {"gui_name": "Comment", "ii_short_description": "mlComment", "ii_order": 26, "measure_code": "ENCS", "contains":"comment"}, 
+    {"gui_name": "Indonesia", "ii_short_description": "ddlFullAp-Indo", "ii_order": 96000000, "measure_code": "Indo", "contains":"Lable"}, 
+    {"gui_name": "Comment", "ii_short_description": "mlComment", "ii_order": 28, "measure_code": "Indo", "contains":"comment"}, 
+    {"gui_name": "Thailand", "ii_short_description": "ddlFullAp-Thai", "ii_order": 100000000, "measure_code": "Thai", "contains":"Lable"}, 
+    {"gui_name": "Comment", "ii_short_description": "mlComment", "ii_order": 30, "measure_code": "Thai", "contains":"comment"}, 
+    {"gui_name": "North America", "ii_short_description": "cbRegionNA", "ii_order": 71000000, "measure_code": "cbRegionNA", "contains":"North America"}, 
+    {"gui_name": "LA", "ii_short_description": "cbRegionLA", "ii_order": 72000000, "measure_code": "cbRegionLA", "contains":"LA"}, 
+    {"gui_name": "EMEA", "ii_short_description": "cbRegionEMEA", "ii_order": 73000000, "measure_code": "cbRegionEMEA", "contains":"EMA"}, 
+    {"gui_name": "AP", "ii_short_description": "cbRegionAP", "ii_order": 74000000, "measure_code": "cbRegionAP", "contains":"AP"}, 
+    {"gui_name": None, "ii_short_description": "mlePlantMatProh", "ii_order": 75000000, "measure_code": "ddlPlantMatProh", "contains":"Plant Proh"}, 
+    {"gui_name": "Proposed Region for Trial", "ii_short_description": "ftPropReg", "ii_order": 77000000, "measure_code": "ftPropReg", "contains":"Lable"}, 
+    {"gui_name": "Proposed region for trial", "ii_short_description": "cbPropRegionTrial", "ii_order": 109000000, "measure_code": "cbPropRegionTrial", "contains":"Lable"}, 
+    {"gui_name": "Proposed Plant for Trial", "ii_short_description": "cbProposedPlant", "ii_order": 79000000, "measure_code": "cbProposedPlant", "contains":"Lable"}, 
+    {"gui_name": "Hidden Spacer", "ii_short_description": "ieAsiaPacificRegion", "ii_order": 107000000, "measure_code": "ieAsiaPacificRegion", "contains":"Lable"},
+    {"gui_name": "Canada", "ii_short_description": "ftFullAp-CEPA", "ii_order": 56000000, "measure_code": "CEPA", "contains":"Lable"}, 
+    {"gui_name": "Comment", "ii_short_description": "mlComment", "ii_order": 70000000, "measure_code": "CEPA", "contains":"comment"},
+    {"gui_name": "SOS Appr. Reg. for Layout", "ii_short_description": "efSOSApprovalReg", "ii_order": 46000000, "measure_code": "", "contains":"SOS Appr. Reg. for Layout"}
+    ,{"gui_name": "Full Approval Prefix Code (hidden)", "ii_short_description": "efFullAppCode", "ii_order": 46000000, "measure_code": "", "contains":"Full Approval Prefix Code (hidden)"}
+]
+
+IC_CAPTION = 'GPS Full Supplier Approval Review'
+IC_ORDER = 7
+
+IC_CAPTION_AGENT = 'GPS Full Agent Approval Review'
+IC_ORDER_AGENT = 5
+
+INFO_FILEDS_FOR_GPS_AGENT = [
+    {"gui_name": "Full Approval Prefix Code (hidden)", "ii_short_description": "efFullAppCode", "ii_order": 46000000, "measure_code": "", "contains":"Full Approval Prefix Code (hidden)"}
+    , {"gui_name": "North America", "ii_short_description": "cbRegionNA", "ii_order": 71000000, "measure_code": "cbRegionNA", "contains":"North America"}
+    , {"gui_name": "LA", "ii_short_description": "cbRegionLA", "ii_order": 72000000, "measure_code": "cbRegionLA", "contains":"LA"}
+    , {"gui_name": "EMEA", "ii_short_description": "cbRegionEMEA", "ii_order": 73000000, "measure_code": "cbRegionEMEA", "contains":"EMA"}
+    , {"gui_name": "AP", "ii_short_description": "cbRegionAP", "ii_order": 74000000, "measure_code": "cbRegionAP", "contains":"AP"}
+]
